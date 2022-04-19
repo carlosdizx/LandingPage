58c22828b9e8f0e9199c66f8bc7e80ecfb56004a
Carlos Díaz <carlos.diaz@ias.com.co>
2022-04-19 00:16:45 -0500

    Eliminado componentes imnecesarios Se creo nuevo componnete para el navbar

---
+++ src/components/NavToolbar.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app-bar absolute color="primary" dark>
+    <v-img
+      class="mx-5"
+      src="@/assets/logo.png"
+      max-width="60"
+      max-height="60"
+      alt="logo"
+    />
+    <v-toolbar-title>Inventas</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-btn icon>
+      <v-icon>mdi-magnify</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-login</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "NavToolbar",
+  data: () => ({}),
+});
+</script>
+
+<style scoped></style>
